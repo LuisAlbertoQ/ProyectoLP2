c557e06ab537c00456d33a76ffa8b4370129437b
neilJoHanCamaCHo <109745928+neilJoHanCamaCHo@users.noreply.github.com>
2023-11-30 17:20:54 -0500

    1

---
+++ database/migrations/2023_11_30_221809_create_announcements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->date('start');
+            $table->date('end');
+            $table->string('description');
+            $table->unsignedBigInteger('coordinator_id');
+            $table->foreign('coordinator_id')->references('id')->on('coordinators')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
