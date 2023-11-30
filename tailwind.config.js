e56b90962903dd783391de9668a404d9098e9adb
LuisAlbertoQ <103071836+LuisAlbertoQ@users.noreply.github.com>
2023-11-30 01:49:53 -0500

    intento-Notification

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ export default {
     presets: [
         require('./vendor/wireui/wireui/tailwind.config.js')
     ],
-
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -16,7 +15,7 @@ export default {
         './resources/views/**/*.blade.php',
         './vendor/wireui/wireui/resources/**/*.blade.php',
         './vendor/wireui/wireui/ts/**/*.ts',
-        './vendor/wireui/wireui/src/View/**/*.php'
+        './vendor/wireui/wireui/src/View/**/*.php',
     ],
 
     theme: {
