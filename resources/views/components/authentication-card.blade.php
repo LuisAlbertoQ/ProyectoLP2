007ee10d5188f57979c74c4995518013f01b2044
LuisAlbertoQ <103071836+LuisAlbertoQ@users.noreply.github.com>
2023-11-30 23:51:33 -0500

    Modificacion-Login

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,6 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="bg-secondary-100 min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-image: url('https://png.pngtree.com/background/20230520/original/pngtree-green-leaves-have-dark-backgrounds-picture-image_2672872.jpg'); background-size: cover; background-position: center;">
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md px-6 py-4 text-white p-3 ps-4 pe-4" style="border: 2px solid rgba(255, 255, 255, 0.2); backdrop-filter: blur(20px);" >
         {{ $slot }}
     </div>
 </div>
